124e19eb7c323555ef5791393edb425653dc1713
riannach <riannach.semple.21@ucl.ac.uk>
2022-02-06 22:22:40 +0000

    last part of part 3

--- cw1/task3/vis.py
+++ cw1/task3/vis.py
@@ -1,6 +1,4 @@
 #%%
-
-from email.errors import BoundaryError
 from classes_etc import trans
 from classes_etc import images
 from classes_etc import rand_im
@@ -25,90 +23,46 @@ import os
 lbt_data = np.load('lbt_file.npy',allow_pickle=False)
 
 # %%
-#print(images)
-title = "man_trans"
-#plot_slices(images[1:2], title='../task3/'+title+str(0), slist = [3,9,17,22,25])
-slist = [3,9,17,22,25]
-#plot_slices(images,"Manual_trans_/Manual Transformation",slist)
-#plot_slices(rand_im,"Random_trans_/Random transforms",slist)
-#plot_slices(r_scal,"Rand_sal_trans_/Strength Parameter",slist)
-
-def plot_new(images, t, pltt, slist):
-    dir = "../task3/"+ t
-    os.mkdir(dir)
-    num = 0
-    for image in images:
-        c=0
-        t_i = t+str(c)
-        fig, ax = plt.subplots(1, 5)
-        fig.set_size_inches(10,2.5) 
-        for slice in slist:
-            array = image[slice]
-            ax[c].imshow(array, cmap='gray',origin='lower', vmin=0, vmax=1)
-            ax[c].title.set_text('Depth = ' + str(slice))
-            c += 1
-            title = dir+"/"+t_i + "_s"+str(slice)+".png"
-        num += 1 
-        plt.suptitle(pltt + str(num))
-        plt.savefig('../task3/'+title+str(num)+'.png', transparent = False)
-        plt.show()
-plot_new(images [1:2],    "Manual_trans_vis", "Manual Transform " ,slist)
-plot_new(rand_im[1:2],   "Random_trans_vis", "Random Transform ",slist)
-plot_new(r_scal [1:2],    "Rand_sal_trans_vis", "Varying Strength Parameter in Random Transform ",slist)
-#%%
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib.colors import LogNorm
-#%%  
-
-#%%
-"""
 # %%
-lbt = lbt_data[15]
-a =lbt[:][0]
-b =lbt[0][:]
-print(a.shape)
 
-#c =lbt_data.shape[2]
-#%%
+lbt_data = np.load('lbt_file.npy',allow_pickle=False)
+im = lbt_data
+dims = np.array([2,.5,.5])
+c =lbt_data.shape[0]
+a =lbt_data.shape[1]
+b =lbt_data.shape[2]
 
-XX, YY = np.meshgrid(a, b, sparse=True)
-
-#%%
-coord = np.concatenate((XX.reshape(-1,1), YY.reshape(-1,1), ZZ.reshape(-1,1)), axis=1)
-maskx = np.random.random(a) <0.3
-masky = np.random.random(b) <0.3
-maskz = np.random.random(c) <0.2
-
-ipts = maskx* x
-jpts = masky* y
-kpts = maskz* z
-
-points = (ipts, jpts, kpts)
-
-image_interpn_flatten = interpn(points, lbt_data, coord, bounds_error=False, fill_value=0)
-image_interpn = image_interpn_flatten.reshape(lbt_data.shape)
-warp = image_interpn
+def func(image,z, x,y):
+    return image[z,x,y]
+x = np.arange(0, a)*dims[1]+.5
+y = np.arange(0, b)*dims[2]+.5
+#y = np.arange(0, c)
+#grid_x, grid_y = np.mgrid[ 0:a, 0:b ]
+grid_x, grid_y = np.meshgrid(x,y)
 
 #%%
-x = #np.arange(0, a)
-y = np.arange(0, b)
-z = np.arange(0, c)
-XX, YY= np.meshgrid(x, y, indexing='ij')#, sparse=True)
+rng = np.random.default_rng()
 
-maskx = np.random.random(a) <0.3
-
+xpts = rng.choice(a, size=4000) #, replace = False)
+ypts = rng.choice(b, size=4000)#, replace = False)
+points = (xpts*dims[1], ypts*dims[2])
 #%%
+from scipy.interpolate import griddata
 
-fig = plt.figure()
-#ax = fig.gca(projection='3d')
-#Z = images[0]
-#surf = ax.plot_surface(XX[0], YY[0], ZZ[1], rstride=1, cstride=1,
- #   linewidth=0, antialiased=False)
+def p_slice(im, t, title, z):
+    values = func(im, z, xpts[:], ypts[:])
+    gris_z0 = griddata(points, values, (grid_x,grid_y), method='linear')
+    plt.imshow(gris_z0.T, origin='lower', aspect=(1))
+    plt.xticks(np.arange(0,128,24),np.arange(0,128,24)*dims[1])
+    plt.yticks(np.arange(0,128,24),np.arange(0,128,24)*dims[1])
+    plt.title(title)
+    plt.savefig("../task3/"+t +"_s" + str(z))
+#p_slice(lbt_data,"slice","Translation", 15)
+#%%
+slist = [5,10,15,20,25]
+for e in range(2):
+    for s in slist:
+        p_slice(images[e],"Man_trans_vis"+str(e),"Manual Translation",s)
+        p_slice(rand_im[e],"Ran_trans_vis"+str(e),"Random Translation",s)
+        p_slice(r_scal[e], "Ran_scal_vis"+str(e),"Random Translation with strength parameter",s)
 
-#%%
-plt.plot(XX,YY)
-print(XX)
-#%%
-# %%
-"""
